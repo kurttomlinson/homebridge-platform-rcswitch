7082970cdbf40cd02f5d3ab2095b11acfe975f27
Kurt Tomlinson <kurttomlinson@gmail.com>
2021-11-18 21:19:52 -0500

    Google format

--- rcswitch.h
+++ rcswitch.h
@@ -3,7 +3,10 @@ extern "C" {
 #endif
 
 #define RCSWITCH_MAX_CHANGES 67
-typedef void (*data_ready_t)(unsigned long received_value, unsigned int received_delay, unsigned int received_protocol, unsigned int received_bit_length, void *userdata);
+typedef void (*data_ready_t)(unsigned long received_value,
+                             unsigned int received_delay,
+                             unsigned int received_protocol,
+                             unsigned int received_bit_length, void *userdata);
 
 void rcswitch_init();
 void rcswitch_set_data_ready_cb(data_ready_t cb, void *data);
@@ -11,9 +14,10 @@ void rcswitch_set_receive_tolerance(int percent);
 void rcswitch_set_protocol(int protocol);
 int rcswitch_enable_receive(int interrupt);
 
-void rcswitch_send(int protocol, int pin, int repeat, int pulse, unsigned long code, unsigned int length);
-void rcswitch_send_scode_word(int protocol, int pin, int repeat, int pulse, char* sCodeWord);
-
+void rcswitch_send(int protocol, int pin, int repeat, int pulse,
+                   unsigned long code, unsigned int length);
+void rcswitch_send_scode_word(int protocol, int pin, int repeat, int pulse,
+                              char *sCodeWord);
 
 #ifdef __cplusplus
 }
